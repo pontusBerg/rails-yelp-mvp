ade32eea210bc388c482de0790f3ec6f4adb416f
Pontus Berg <pontusberg95@gmail.com>
2019-06-26 17:21:12 +0900

    done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,52 @@
+class RestaurantsController < ApplicationController
+
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+      if @restaurant.save
+        redirect_to restaurant_path(@restaurant)
+      else
+        render :new
+      end
+    end
+
+  def edit
+    # Relevant code.
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurants_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+Restaurant.create(name: 'Ilanda Pizzeria', address: "I sköre", phone_number: 15647,
+category: "Pizza" )
+
+Restaurant.create(name: 'Norrstrand Pizzeria', address: "I Nörrstrand", phone_number: 156356,
+category: "Pizza" )
+
+Restaurant.create(name: 'Spicy Hot', address: "Stora Torget", phone_number: 15642,
+category: "Asian" )
+
+Restaurant.create(name: 'Downtown', address: "Herrhagen", phone_number: 25646,
+category: "Pizza" )
